c4e46edb7f0cd40944f2a540f50c68a06bc8387e
liujc <liujc623@gmail.com>
2021-01-10 20:57:46 +0000

    add stem utility

---
+++ AbstractSearchEngine/utils/stemmer.py
@@ -0,0 +1,14 @@
+from Stemmer import Stemmer
+from AbstractSearchEngine.db.StemHistory import update_stem_history, query_origin_word
+
+stemmer = Stemmer('porter')
+
+
+def stem(term):
+    stemmed_word = stemmer.stemWord(term)
+    update_stem_history(term, stemmed_word)
+    return stemmed_word
+
+
+def unstem(stemmed_term):
+    return query_origin_word(stemmed_term)
