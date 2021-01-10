26cc1c86927e668ae3b9dd23784866fbc623f0ac
liujc <liujc623@gmail.com>
2021-01-10 12:40:48 +0000

    switch to postgresql db

--- arXivExplorer/settings.py
+++ arXivExplorer/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'arXivExplorer.wsgi.application'
 # The configure file is in arXivExplorer folder. (The same as manage.py)
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config['pg_dbname'],
+        'USER': config['pg_username'],
+        'PASSWORD': config['pg_password'],
+        'HOST': config['pg_host'],
+        'PORT': config['pg_port'],
     }
 }
 
