5afb08d0a891817c04d12deb61eb42cfae410115
liujc <liujc623@gmail.com>
2021-01-11 13:13:11 +0000

    add function to get document detail

---
+++ AbstractSearchEngine/db/arXivDocument.py
@@ -0,0 +1,8 @@
+def get_all_arxiv_documents():
+    # return all document in the database, including arxiv_id and abstract.
+    return
+
+
+def get_arxiv_document_by_id(arxiv_id):
+    # return the document that matched arxiv_id, or None if not found.
+    return
