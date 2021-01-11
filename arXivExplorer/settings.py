06cf6111ebac7e68693bdeb206765cab3c72a788
liujc <liujc623@gmail.com>
2021-01-11 23:24:27 +0000

    add app to setting

--- arXivExplorer/settings.py
+++ arXivExplorer/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'AbstractSearchEngine',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
