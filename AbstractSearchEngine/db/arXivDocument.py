70815531361abecc0679911d1292d18d73f473dd
Ivy Ruan <ivyruan1997@gmail.com>
2021-01-13 10:45:34 +0000

    Update arXivDocument.py
    get corresponding document metadata from database

--- AbstractSearchEngine/db/arXivDocument.py
+++ AbstractSearchEngine/db/arXivDocument.py
@@ -1,8 +1,18 @@
+from AbstractSearchEngine.models import ArxivDocument
+# ArxivDocument model gives all the metadata:
+# arxiv_id / submitter / authors / title / comments / doi / report_no / categories /
+# journal_ref / license / abstract / versions / update_date / authors_parsed
+
+# the following 2 functions return all the metadata
 def get_all_arxiv_documents():
     # return all document in the database, including arxiv_id and abstract.
-    return
+    # return type: QuerySet
+    return ArxivDocument.objects.all()
 
 
 def get_arxiv_document_by_id(arxiv_id):
     # return the document that matched arxiv_id, or None if not found.
-    return
+    if ArxivDocument.objects.filter(arxiv_id=arxiv_id).exists():
+        return ArxivDocument.objects.get(arxiv_id=arxiv_id)
+    else:
+        return None
