212a8631ed4e1b2fa4b7130da443c0be553e500c
Ivy Ruan <ivyruan1997@gmail.com>
2021-01-13 10:44:26 +0000

    Update StemHistory.py
    store stemmed-word pairs in database

--- AbstractSearchEngine/db/StemHistory.py
+++ AbstractSearchEngine/db/StemHistory.py
@@ -1,24 +1,24 @@
-import json
-
+from AbstractSearchEngine.models import StemPair
+# StemPair model has 2 columns: key -> word after stemming / value -> original word
 
 def update_stem_history(word, stemmed_word):
     """
     please design a table that has two columns, column 1 is stemmed word while column 2 is original word.
     """
-    file = open('stemhistory.txt', 'r')
-    k_v = json.load(file)
-    file.close()
-    k_v[stemmed_word] = word
-    file = open('stemhistory.txt', 'w')
-    json.dump(file, k_v)
-    file.close()
+    if StemPair.objects.filter(key=stemmed_word).exists():
+        # already have the stemmed word pair, update the value
+        temp = StemPair.objects.get(key=stemmed_word)
+        temp.value = word
+        temp.save()
+    else:
+        # create a new pair
+        temp = StemPair(
+            value=word,
+            key=stemmed_word
+        )
+        temp.save()
 
 
 def query_origin_word(stemmed_word):
-    file = open('stemhistory.txt', 'r')
-    k_v = json.load(file)
-    file.close()
-    if stemmed_word in k_v:
-        return k_v[stemmed_word]
-    else:
-        return ""
+    # return the latest original word
+    return StemPair.objects.get(key=stemmed_word).value
