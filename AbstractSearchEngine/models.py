c1c7f06e1590057477da495ddc70097d99e833bd
Ivy Ruan <ivyruan1997@gmail.com>
2021-01-13 10:41:52 +0000

    Update models.py
    Upater models for 3 tables: main table arxiv_document / randed retrieval table arxiv_rank / table to store stemmed word stem_pair

--- AbstractSearchEngine/models.py
+++ AbstractSearchEngine/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class ArxivDocument(models.Model):
+    arxiv_id = models.TextField(unique=True, blank=True, null=True)
+    submitter = models.TextField(blank=True, null=True)
+    authors = models.TextField(blank=True, null=True)
+    title = models.TextField(blank=True, null=True)
+    comments = models.TextField(blank=True, null=True)
+    doi = models.TextField(blank=True, null=True)
+    report_no = models.TextField(blank=True, null=True)
+    categories = models.TextField(blank=True, null=True)
+    journal_ref = models.TextField(blank=True, null=True)
+    license = models.TextField(blank=True, null=True)
+    abstract = models.TextField(blank=True, null=True)
+    versions = models.TextField(blank=True, null=True)  # This field type is a guess.
+    update_date = models.TextField(blank=True, null=True)
+    authors_parsed = models.TextField(blank=True, null=True)  # This field type is a guess.
+
+    class Meta:
+        managed = False
+        db_table = 'arxiv_document'
+
+class ArxivRank(models.Model):
+    word = models.CharField(max_length=20, blank=True, null=True)
+    paper = models.ForeignKey(ArxivDocument, models.DO_NOTHING, blank=True, null=True)
+    algorithm = models.CharField(max_length=20, blank=True, null=True)
+    rank_value = models.FloatField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'arxiv_rank'
+        unique_together = (('word', 'paper', 'algorithm'),)
+
+class StemPair(models.Model):
+    key = models.CharField(primary_key=True, max_length=20)
+    value = models.CharField(max_length=20, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'stem_pair'
