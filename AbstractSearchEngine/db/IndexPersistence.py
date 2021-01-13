557b7adf2875d7be5caaf87ffd7c7502c5e243c8
Ivy Ruan <ivyruan1997@gmail.com>
2021-01-13 10:48:24 +0000

    Update IndexPersistence.py
    store the ranked retrieval values in arxiv_rank table

--- AbstractSearchEngine/db/IndexPersistence.py
+++ AbstractSearchEngine/db/IndexPersistence.py
@@ -2,8 +2,9 @@
 
 # These example function simply store index in file,
 # please keep the returns and behaviors identical, but store them in SQL.
-import os
 
+from AbstractSearchEngine.models import ArxivRank
+# ArxivRank model has 4 columns: word / paper(arvix_id) / algorithm / rank_value
 
 def set_index(key1: str, key2: str, key3: str, value) -> bool:
     """
@@ -15,13 +16,22 @@ def set_index(key1: str, key2: str, key3: str, value) -> bool:
                   It is wiser to store them in string format in db.
     :return bool: return if the operation successes.
     """
+
     try:
-        if os.path.exists('indexTemp'):
-            pass
+        if ArxivRank.objects.filter(paper=key1, word=key2, algorithm=key3).exists():
+            # already have the stemmed word pair, update the value
+            temp = ArxivRank.objects.get(paper=key1, word=key2, algorithm=key3)
+            temp.rank_value = value
+            temp.save()
         else:
-            os.mkdir('indexTemp')
-        with open('indexTemp/' + key1 + '-' + key2 + '-' + key3, 'w') as fout:
-            fout.write(value)
+            # create a new row
+            temp = ArxivRank(
+                paper=key1,
+                word=key2,
+                algorithm=key3,
+                rank_value=value
+            )
+            temp.save()
         return True
     except:
         return False
@@ -36,23 +46,24 @@ def get_index(key1: str, key2: str, key3: str) -> float:
     :return value: The value of key. Note that we return a float value because we use TFIDF or BM25.
     """
     try:
-        with open('indexTemp/' + key1 + '-' + key2 + '-' + key3, 'r') as fin:
-            return float(fin.read())
+        temp = ArxivRank.objects.get(paper=key1, word=key2, algorithm=key3)
+        return temp.rank_value
     except:
         return 0
 
 
 def delete_index(key1: str, key2: str, key3: str) -> bool:
     """
-    Delete index. Notice that we can set the value to 0 instead physically delete the file on disk.
-    However, it might be unwise set value to 0 on db for it may result in a huge sparse table.
+    Delete index. If the row with key(key1,key2,key3) exists, delete the row in db.
     :param key1: index key 1 (for example arxiv_id)
     :param key2: index key 2 (for example the term)
     :param key3: index key 3 (for example the algorithm(TFIDF or BM25))
     :return bool: return if the operation successes.
     """
     try:
-        return set_index(key1, key2, key3, 0)
+        temp = ArxivRank.objects.get(paper=key1, word=key2, algorithm=key3)
+        temp.delete()
+        return True
     except:
         return False
 
@@ -71,18 +82,15 @@ def delete_all_index(key1=None, key2=None, key3=None):
     if key1 is None and key2 is None and key3 is None:
         return True
     try:
-        files = os.listdir('indexTemp/')
-        for file in files:
-            fkey1, fkey2, fkey3 = file.split('-')
-            flag = True
-            if key1 is not None:
-                flag = flag | (fkey1 == key1)
-            if key2 is not None:
-                flag = flag | (fkey2 == key2)
-            if key3 is not None:
-                flag = flag | (fkey3 == key3)
-            if flag:
-                set_index(fkey1, fkey2, fkey3, 0)
+        temp = ArxivRank.objects
+        if key1 is not None:
+            temp = temp.filter(paper=key1)
+        if key2 is not None:
+            temp = temp.filter(word=key2)
+        if key3 is not None:
+            temp = temp.filter(algorithm=key3)
+        for i in temp:
+            i.delete()
     except:
         return False
 
@@ -101,17 +109,15 @@ def get_all_index(key1=None, key2=None, key3=None):
     if key1 is None and key2 is None and key3 is None:
         return []
     try:
-        files = os.listdir('indexTemp/')
-        for file in files:
-            fkey1, fkey2, fkey3 = file.split('-')
-            flag = True
-            if key1 is not None:
-                flag = flag | (fkey1 == key1)
-            if key2 is not None:
-                flag = flag | (fkey2 == key2)
-            if key3 is not None:
-                flag = flag | (fkey3 == key3)
-            if flag:
-                ret_list.append([key1, key2, key3, get_index(key1, key2, key3)])
+        temp = ArxivRank.objects
+        if key1 is not None:
+            temp = temp.filter(paper=key1)
+        if key2 is not None:
+            temp = temp.filter(word=key2)
+        if key3 is not None:
+            temp = temp.filter(algorithm=key3)
+        for i in temp:
+            ret_list.append(i.paper)
+        return ret_list
     except:
         return []
