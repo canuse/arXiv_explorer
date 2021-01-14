ffdbd653546ae9494a5268929d2d7a11ec4c59e3
liujc <liujc623@gmail.com>
2021-01-14 12:54:48 +0000

    add unified search

---
+++ AbstractSearchEngine/indexing/UnifiedSearch.py
@@ -0,0 +1,10 @@
+def search_by_words(word_list):
+    return []
+
+
+def query_expansion(word_list, nrel=10, nexp=2, allow_dup=True):
+    return []
+
+
+def get_relative_article(arxivID_list, nart=10):
+    return []
