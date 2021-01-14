e3c17e75a481372f9b5b2ef9e2210a7906eb2331
bytedance <bytedance@c02d82drml85.googleapis.com>
2021-01-14 15:09:08 +0800

    start api app

--- AbstractSearchEngine/tests.py
+++ AbstractSearchEngine/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.test.client import Client
+from .models import *
 
 # Create your tests here.
+
+class ArxivDocumentTests(TestCase):
+
+    def test_getDatial():
+        from api import getDatial
+        
+        b = ArxivDocument(arxiv_id='abc',
+                        title='hello world')
+        b.save()
+        
+        c = Client()
+        req = c.get('abc')
+        
+        assert(b.get(req) == 'hello world')
