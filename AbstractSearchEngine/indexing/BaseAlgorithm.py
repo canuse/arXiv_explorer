6f20b23324a1cb19c916e282ca02e0f0f4229e60
liujc <liujc623@gmail.com>
2021-01-16 23:47:41 +0000

    add comment of base algorithm

--- AbstractSearchEngine/indexing/BaseAlgorithm.py
+++ AbstractSearchEngine/indexing/BaseAlgorithm.py
@@ -2,27 +2,55 @@ from abc import ABC, abstractmethod
 
 
 class BaseAlgorithm(ABC):
+    """
+    Interface of algorithm class, every indexing algorithm should implement the following functions.
+    """
     def __init__(self):
         pass
 
     @staticmethod
     @abstractmethod
     def update_index(index):
+        """
+        Update the index (by using the given index, whose format is BaseIndex).
+        Only update part of the whole index, to save time.
+        """
         return False
 
     @staticmethod
     @abstractmethod
     def search_by_words(word_list):
+        """
+        searching using the index and algorithm
+        word_list: a list of stemmed, tokenized word
+        return: a list of tuple, each tuple contains the arxiv_id and relevance
+        example: [('0901.0001',12.34),('0902.0002',23.45)]
+        """
         return []
 
     @staticmethod
     @abstractmethod
     def query_expansion(word_list, nrel=10, nexp=2, allow_dup=True):
+        """
+        expand the query (to get more result)
+        word_list: a list of stemmed, tokenized word
+        nrel: number of documents considered as relative
+        nexp: number of extend query word
+        allow_dup: allow duplicate in expanded query words
+        return: a list of word, containing original word list and expanded words
+        example: input: ['appl', 'orang'] output: ['appl', 'orang', 'banana', 'fruit']
+        """
         return []
 
     @staticmethod
     @abstractmethod
     def get_relative_article(arxivID_list, nart=10):
+        """
+        Get relevance articles of the given arxiv_id list.
+        arxivID_list: a list of arxiv id
+        nart: number of relative articles
+        return: a list of tuple, containing arxiv_id and relevance score
+        """
         return []
 
 
