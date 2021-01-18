13ad6e788a36a32e70ec1d140df2a3525e30a7f0
jiaoyunpeng <1300820524@qq.com>
2021-01-18 22:47:31 +0800

    try catch

--- AbstractSearchEngine/views/api.py
+++ AbstractSearchEngine/views/api.py
@@ -1,4 +1,5 @@
 import datetime
+import traceback
 import json
 from ..models import *
 from ..db.arXivDocument import *
@@ -16,30 +17,33 @@ def getDatial(request):
     Returns:
         json: 该article的metadata，调用ORM接口查数据库
     """
-    ret_dict = {}
-    
-    arxiv_id = request.GET.get("arxivID")
-    arxiv_doc = get_arxiv_document_by_id(arxiv_id=arxiv_id)
-    
-    if arxiv_doc != None:
-        request.session.set['last_read'] = arxiv_id
+    try:
+        ret_dict = {}
         
-        ret_dict['arxiv_id'] = arxiv_doc.arxiv_id
-        ret_dict['submitter'] = arxiv_doc.submitter
-        ret_dict['authors'] = arxiv_doc.authors
-        ret_dict['title'] = arxiv_doc.title
-        ret_dict['comments'] = arxiv_doc.comments
-        ret_dict['doi'] = arxiv_doc.doi
-        ret_dict['report_no'] = arxiv_doc.report_no
-        ret_dict['categories'] = arxiv_doc.categories
-        ret_dict['journal_ref'] = arxiv_doc.journal_ref
-        ret_dict['license'] = arxiv_doc.license
-        ret_dict['abstract'] = arxiv_doc.abstract
-        ret_dict['versions'] = arxiv_doc.versions
-        ret_dict['update_date'] = arxiv_doc.update_date
-        ret_dict['authors_parsed'] = arxiv_doc.authors_parsed
+        arxiv_id = request.GET.get("arxivID")
+        arxiv_doc = get_arxiv_document_by_id(arxiv_id=arxiv_id)
         
-    return json.dump(ret_dict)
+        if arxiv_doc != None:
+            request.session.set['last_read'] = arxiv_id
+            
+            ret_dict['arxiv_id'] = arxiv_doc.arxiv_id
+            ret_dict['submitter'] = arxiv_doc.submitter
+            ret_dict['authors'] = arxiv_doc.authors
+            ret_dict['title'] = arxiv_doc.title
+            ret_dict['comments'] = arxiv_doc.comments
+            ret_dict['doi'] = arxiv_doc.doi
+            ret_dict['report_no'] = arxiv_doc.report_no
+            ret_dict['categories'] = arxiv_doc.categories
+            ret_dict['journal_ref'] = arxiv_doc.journal_ref
+            ret_dict['license'] = arxiv_doc.license
+            ret_dict['abstract'] = arxiv_doc.abstract
+            ret_dict['versions'] = arxiv_doc.versions
+            ret_dict['update_date'] = arxiv_doc.update_date
+            ret_dict['authors_parsed'] = arxiv_doc.authors_parsed
+            
+        return json.dump(ret_dict)
+    except Exception:
+        traceback.print_exc()
 
 
 def getRecommendArticle(request):
@@ -53,11 +57,14 @@ def getRecommendArticle(request):
     Returns:
         list，最接近的文章的id（比如默认10个）
     """    
-    arxiv_id = request.GET.get("arxivID")
-    if arxiv_id == '':
-        arxiv_id = request.session.get('last_read', '-1')
-        # TODO: 新用户没有搜索记录
-    return get_relative_article(arxiv_list=[arxiv_id], nart=10)  # TODO:参数含义
+    try:
+        arxiv_id = request.GET.get("arxivID")
+        if arxiv_id == '':
+            arxiv_id = request.session.get('last_read', '-1')
+            # TODO: 新用户没有搜索记录
+        return get_relative_article(arxiv_list=[arxiv_id], nart=10)  # TODO:参数含义
+    except Exception:
+        traceback.print_exc()
 
 
 def query(request):
@@ -78,72 +85,75 @@ def query(request):
         {[(article_id, title, abstract, authors, update_date)*20]，50}
         表示一共有50个搜索结果，本次查询返回的20个结果是上面显示的20个
     """   
-    ret_list = []
-    ret_dict = {'ret_list': ret_list, 'num': 0}
-    
-    # 解析request信息
-    query_string_raw = request.GET.get("queryString")
-    categories_raw = request.GET.get("categories")
-    time_start_raw = timeConvert(request.GET.get("timeStart"))
-    time_end_raw = timeConvert(request.GET.get("timeEnd"))
-    offset = request.GET.get("offset")
-    
-    # 时间提取 # TODO:如果传入时间为空怎么办
-    time_start_year = time_start_raw[:4]
-    time_start_month = time_start_raw[-2:]
-    time_end_year = time_end_raw[:4]
-    time_end_month = time_end_raw[-2:]
-    
-    # category info extraction
-    categories = categories_raw.split(',') # TODO: 空类别
-    
-    # preprocess and stemming
-    query_string_list = [stem(query) for query in preprocess(query_string_raw)]
-    
-    # return arxiv_ids by search words
-    arxiv_ids = search_by_words(word_list=query_string_list)
-    
-    # return arxiv_docs by arxiv_ids
-    arxiv_docs = get_arxiv_document_by_ids(arxiv_ids)
-    
-    # 条件筛选
-    for doc in arxiv_docs:
-        flag = True
+    try:
+        ret_list = []
+        ret_dict = {'ret_list': ret_list, 'num': 0}
         
-        # 使用文章类别筛选
-        if (len(categories) == 0) or (doc.categories in categories):
-            flag = flag and True
+        # 解析request信息
+        query_string_raw = request.GET.get("queryString")
+        categories_raw = request.GET.get("categories")
+        time_start_raw = timeConvert(request.GET.get("timeStart"))
+        time_end_raw = timeConvert(request.GET.get("timeEnd"))
+        offset = request.GET.get("offset")
+        
+        # 时间提取 # TODO:如果传入时间为空怎么办
+        time_start_year = time_start_raw[:4]
+        time_start_month = time_start_raw[-2:]
+        time_end_year = time_end_raw[:4]
+        time_end_month = time_end_raw[-2:]
+        
+        # category info extraction
+        categories = categories_raw.split(',') # TODO: 空类别
+        
+        # preprocess and stemming
+        query_string_list = [stem(query) for query in preprocess(query_string_raw)]
+        
+        # return arxiv_ids by search words
+        arxiv_ids = search_by_words(word_list=query_string_list)
+        
+        # return arxiv_docs by arxiv_ids
+        arxiv_docs = get_arxiv_document_by_ids(arxiv_ids)
+        
+        # 条件筛选
+        for doc in arxiv_docs:
+            flag = True
+            
+            # 使用文章类别筛选
+            if (len(categories) == 0) or (doc.categories in categories):
+                flag = flag and True
+            else:
+                flag = False
+            
+            # 使用发表年、月筛选
+            # TODO:如果doc的update_date为空怎么办
+            doc_year = doc.update_date.split('-')[0]
+            doc_month = doc.update_date.split('-')[1]
+            if (time_start_year == doc_year) and (time_start_month <= doc_month):
+                flag = flag and True
+            elif (time_end_year == doc_year) and (doc_month <= time_end_month):
+                flag = flag and True
+            elif time_start_year <= doc_year <= time_end_year:
+                flag = flag and True
+            else:
+                flag = False
+            
+            if flag:
+                ret_list.append((doc.article_id, doc.title,
+                            doc.abstract, doc.authors, doc.update_date))
+        
+        ret_dict['num'] = len(ret_list)
+        
+        # 边界条件
+        if len(ret_list) <= offset:
+            ret_dict['ret_list'] = ret_list[:]
+        elif offset < len(ret_list) <= (offset+20):
+            ret_dict['ret_list'] = ret_list[offset:]
         else:
-            flag = False
+            ret_dict['ret_list'] = ret_list[offset:offset+20]
         
-        # 使用发表年、月筛选
-        # TODO:如果doc的update_date为空怎么办
-        doc_year = doc.update_date.split('-')[0]
-        doc_month = doc.update_date.split('-')[1]
-        if (time_start_year == doc_year) and (time_start_month <= doc_month):
-            flag = flag and True
-        elif (time_end_year == doc_year) and (doc_month <= time_end_month):
-            flag = flag and True
-        elif time_start_year <= doc_year <= time_end_year:
-            flag = flag and True
-        else:
-            flag = False
-        
-        if flag:
-            ret_list.append((doc.article_id, doc.title,
-                         doc.abstract, doc.authors, doc.update_date))
-    
-    ret_dict['num'] = len(ret_list)
-    
-    # 边界条件
-    if len(ret_list) <= offset:
-        ret_dict['ret_list'] = ret_list[:]
-    elif offset < len(ret_list) <= (offset+20):
-        ret_dict['ret_list'] = ret_list[offset:]
-    else:
-        ret_dict['ret_list'] = ret_list[offset:offset+20]
-    
-    return json.dump(ret_dict)
+        return json.dump(ret_dict)
+    except Exception:
+        traceback.print_exc()
 
 
 def queryExpansion(request):
@@ -158,24 +168,28 @@ def queryExpansion(request):
         一个list，包括QE后的字符串（比如默认10个）
         例如，输入 ”apple banana“，返回["apple banana fruit","apple banana orange".....]
     """
-    ret_list = []
-    
-    # 解析request信息
-    query_string_raw = request.GET.get("queryString")
-    
-    # preprocess and stemming
-    query_string_list = [stem(query) for query in preprocess(query_string_raw)]
+    try:
+        ret_list = []
+        
+        # 解析request信息
+        query_string_raw = request.GET.get("queryString")
+        
+        # preprocess and stemming
+        query_string_list = [stem(query) for query in preprocess(query_string_raw)]
 
-    # return query_string_expanded_list by search words
-    query_string_expanded_list = query_expansion(
-        word_list=query_string_list, nrel=10, nexp=2, allow_dup=True)#TODO:参数含义
-    
-    #TODO:是这么搞unstem么
-    for query in query_string_expanded_list:
-        words_unstem = ''
-        words = query.split(' ')
-        for w in words:
-            words_unstem += unstem(w) + ' '
-        ret_list.append(words_unstem[:-1])
-    
-    return ret_list
+        # return query_string_expanded_list by search words
+        query_string_expanded_list = query_expansion(
+            word_list=query_string_list, nrel=10, nexp=2, allow_dup=True)#TODO:参数含义
+        
+        #TODO:是这么搞unstem么
+        for query in query_string_expanded_list:
+            words_unstem = ''
+            words = query.split(' ')
+            for w in words:
+                words_unstem += unstem(w) + ' '
+            ret_list.append(words_unstem[:-1])
+        
+        return ret_list
+
+    except Exception:
+        traceback.print_exc()
