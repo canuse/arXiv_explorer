1ca67dc3d7947180f0a3465f85280f3d11fc516f
jiaoyunpeng <1300820524@qq.com>
2021-02-04 22:14:43 +0800

    complete urls info

--- arXivExplorer/urls.py
+++ arXivExplorer/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ..AbstractSearchEngine.views import api
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('getDatial/', api.getDatial),
+    path('getRecommendArticle/', api.getRecommendArticle),
+    path('query/', api.query),
+    path('queryExpansion/', api.queryExpansion),
 ]
