026dad2b59d1843ee5227511caf5deb3e7eddee2
Wenzhi Zhang <s2016083@ed.ac.uk>
2021-02-15 16:08:06 +0000

    获取stempair表内容

--- AbstractSearchEngine/db/getStemPair.py
+++ AbstractSearchEngine/db/getStemPair.py
@@ -2,7 +2,5 @@ from AbstractSearchEngine.models import StemPair
 
 def get_stem_pair_by_key(key_head):
     # return the document that matched key_head, or None if not found.
-    if StemPair.objects.filter(key.startswith(key_head)).exists():
-        return StemPair.objects.get(key.startswith(key_head))
-    else:
-        return None
+    return StemPair.objects.filter(key.startswith(key_head))
+
